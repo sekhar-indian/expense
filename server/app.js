38aab729b26c8924fefa4f3c0e2a7fdeeff6b3b6
sekhar <munisekharudavalapati1@gmail.com>
2024-07-11 13:06:08 +0530

    s

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
 const express=require('express');
 const app=express();
 const cors=require('cors');
+const dotenv=require('dotenv');
+dotenv.config();
 const bodyParser=require('body-parser');
 const sequelize=require('./util/dbConection');
 const Userdb=require('./models/signup');
