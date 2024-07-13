85a87f480e6d6e5c4d19baad9e0dbe0f4c05fc6e
sekhar <munisekharudavalapati1@gmail.com>
2024-07-13 19:45:57 +0530

    static file update

--- server/app.js
+++ server/app.js
@@ -9,7 +9,7 @@ const Userdb=require('./models/signup');
 const Expense=require('./models/expense');
 const Orders=require('./models/orders');
 const s3file=require('./models/s3files');
-
+const path = require('path'); 
 // const admin=require('./routers/admin');
 const user=require('./routers/user');
 app.use(cors());
@@ -19,13 +19,16 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // app.use(admin);
 app.use(user);
 
+app.use((req,res)=>{
+    res.sendFile(path.join(__dirname,`public/${req.url}`))
+})
+
 Expense.belongsTo(Userdb,{constraints:true,onDelete:'CASCADE'});
 Userdb.hasMany(Expense)
 Orders.belongsTo(Userdb,{constraints:true,onDelete:'CASCADE'});
 Userdb.hasMany(Orders)
 s3file.belongsTo(Userdb,{constraints:true,onDelete:'CASCADE'});
 Userdb.belongsTo(s3file)
- 
 sequelize
     .sync()
     .then(res=>{
